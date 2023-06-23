466fadface8dfcdb7b988ea66b27b8eedb89c882
T Nagabhushanam Reddy <nagareddy318@gmail.com>
2023-06-23 12:50:16 +0530

    Add files via upload

--- model_methods.py
+++ model_methods.py
@@ -1,12 +1,39 @@
+import re
+import pickle
+import joblib
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-import pickle
+from sklearn.pipeline import Pipeline, FunctionTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+def clean_series(x):
+    return x.apply(clean_data)
+def clean_data(text):
+    text = text.lower() # text to lowercase
+    text = re.sub(r'\s\{\$\S*', '',text) # Remove text within curly braces
+    text = re.sub(r'\n', '', text) # Remove line breaks
+    text = re.sub(r'\(\w*\)', '', text) #remove text within braces
+    text = re.sub(r'(\W\s)|(\W$)|(\W\d*)', ' ',text) # Remove punctuation
+    text = re.sub(r'x+((/xx)*/\d*\s*)|x*', '',text) #Remove date
+    text = re.sub(r'\d+\s', '', text) #Remove other numerical values
+    text = re.sub(r' +', ' ',text) #Remove unnecessary white spaces
+    return text
+
 def predict(arr):
     # Load the model
-    with open('finalized_model.sav', 'rb') as f:
-        model = pickle.load(f)
+    model = pickle.load(open('finalized_model.sav', 'rb'))
     classes = {0:'Account Services', 1:'Others', 2:'Mortgage/Loan', 3:'Credit card or prepaid card', 4:'Theft/Dispute Reporting'}
     # return prediction as well as class probabilities
-    preds = model.predict_proba([arr])[0]
+    preprocessor = joblib.load('preprocessor.pkl')
+    prepro_text = preprocessor.transform(pd.Series([arr])) 
+    preds = model.predict_proba(prepro_text)[0]
     return (classes[np.argmax(preds)], preds)
+
+
+
+
+
+
+
